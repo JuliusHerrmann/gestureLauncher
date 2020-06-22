65d07959a0c69de20531f74579e0f5c246be5c4e
JuliusHerrmann <herrmann.julius@gmx.de>
2020-06-22 23:04:46 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/myLauncher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myLauncher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+// Copyright 2017 Ashraff Hathibelagal.
+// Use of this source code is governed by an Apache license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:launcher_assist/launcher_assist.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var numberOfInstalledApps;
+  var installedApps;
+  var wallpaper;
+
+  @override
+  initState() {
+    super.initState();
+
+    requestPermission(Permission.storage);
+
+    // Get all apps
+    LauncherAssist.getAllApps().then((apps) {
+      setState(() {
+        numberOfInstalledApps = apps.length;
+        installedApps = apps;
+      });
+    });
+
+    // Get wallpaper as binary data
+    LauncherAssist.getWallpaper().then((imageData) {
+      setState(() {
+        wallpaper = imageData;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Launcher Assist'),
+          ),
+          body: new Column(children: <Widget>[
+            new Text("Found $numberOfInstalledApps apps installed"),
+            new RaisedButton(
+                child: new Text("Launch Something"),
+                onPressed: () {
+                  // Launch the first app available
+                  LauncherAssist.launchApp(installedApps[1]["package"]);
+                }),
+            wallpaper != null
+                ? new Image.memory(wallpaper, fit: BoxFit.scaleDown)
+                : new Center()
+          ])),
+    );
+  }
+
+  Future<void> requestPermission(Permission permission) async {
+    final status = await permission.request();
+
+    setState(() {
+      print(status);
+    });
+  }
+}
